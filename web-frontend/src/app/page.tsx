f39e52b3562b8b885683dd4b8525f4f9c224b06b
Bryan Hertelt <bryanhertelt@gmail.com>
2025-10-09 13:46:07 +0200

    fix: Add flag to page component.

--- web-frontend/src/app/page.tsx
+++ web-frontend/src/app/page.tsx
@@ -1,5 +1,8 @@
 import CryptoDashboard from "@/pages/dashboard/dashboard-page";
 
+// Force dynamic rendering at request time
+export const dynamic = "force-dynamic";
+
 export default function CryptoDashboardPage() {
   return <CryptoDashboard />;
 }
