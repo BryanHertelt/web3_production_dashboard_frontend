fa905353f221619691bed1aa9f23d0716de04da9
Bryan Hertelt <bryanhertelt@gmail.com>
2025-11-16 11:05:50 +0100

    feat: Fix typescript types and implement mock dashboard.

---
+++ web-frontend/src/app/data-table-test/page.tsx
@@ -0,0 +1,5 @@
+import DataTableTestComponent from "@/page-components/datatable-test";
+
+export default function DataTablePage() {
+  return <DataTableTestComponent />;
+}
--- web-frontend/src/entities/asset/model/types.ts
+++ web-frontend/src/entities/asset/model/types.ts
@@ -3,11 +3,14 @@ import { UseQueryOptions } from "@tanstack/react-query";
 
 export interface Asset {
   asset_id: string;
+  asset_type: string;
   symbol: string;
   icon: string;
   name: string;
-  abs_value_change: string;
-  rel_value_change: string;
+  abs_price_change_daily: number;
+  rel_price_change_daily: number;
+  abs_profit_loss: number;
+  rel_profit_loss: number;
   amount: number;
   abs_value: number;
   allocation_percent: number;
@@ -16,7 +19,7 @@ export interface Asset {
 export interface AssetInfo {
   portfolio_id: string;
   data: Asset[];
-  abs_total_val: number;
+  abs_total_value: number;
 }
 
 //Query hook types
