0fab033623f5eaeb84d035a91a0b27e24e375088
Bryan Hertelt <bryanhertelt@gmail.com>
2025-11-17 18:43:03 +0100

    refactor: Refactor component structure and apply loading skeleton.

--- web-frontend/src/app/data-table-test/page.tsx
+++ web-frontend/src/app/data-table-test/page.tsx
@@ -1,14 +1,9 @@
-import DataTableTestComponent from "@/page-components/datatable-test";
+import { DataTable } from "@/features/data-table";
 
 export default function DataTablePage() {
   return (
     <div className="flex flex-row w-screen h-screen items-center justify-center bg-black">
-      <div className="bg-base-dark-bg w-1/2 h-1/2 p-4 border-2 border-color-neutral-350 rounded-md">
-        <div className="w-full h-full overflow-scroll">
-          {" "}
-          <DataTableTestComponent />{" "}
-        </div>
-      </div>
+      <DataTable />
     </div>
   );
 }
--- web-frontend/src/features/data-table/ui/data-table.tsx
+++ web-frontend/src/features/data-table/ui/data-table.tsx
@@ -1,7 +1,42 @@
-import { assetColumns } from "../lib/cryptocurrency-columns";
-import { DataTableProps } from "../model/type";
-import { CoreTable } from "@/shared/core-table";
+"use client";
 
-export const DataTable = (props: DataTableProps) => {
-  return <CoreTable columns={assetColumns} data={props.data} />;
+import { assetColumns } from "../lib/cryptocurrency-columns";
+import { CoreTable } from "@/shared/core-table";
+import { useAssetInfo } from "@/entities/asset";
+import { Spinner } from "@/shared/ui/feedback/spinner";
+
+export const DataTable = () => {
+  const { data, isLoading, isError } = useAssetInfo({
+    portfolioId: "shc34",
+    query: {},
+  });
+
+  const tableCard =
+    "bg-base-dark-bg w-1/2 h-1/2 p-4 border-2 border-color-neutral-350 rounded-md";
+
+  if (isLoading) {
+    return (
+      <div className={`${tableCard} flex flex-row justify-center items-center`}>
+        {" "}
+        <Spinner />
+      </div>
+    );
+  }
+
+  if (isError) {
+    return <> Error...</>;
+  }
+
+  if (!data) {
+    return <> Data is undefined...</>;
+  }
+
+  return (
+    <div className={`${tableCard}`}>
+      <div className="w-full h-full overflow-scroll">
+        {" "}
+        <CoreTable columns={assetColumns} data={data.data} />{" "}
+      </div>
+    </div>
+  );
 };
