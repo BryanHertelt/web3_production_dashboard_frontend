fea5a6edb48c057fcd7b6b03ee1701d3e6612755
Bryan Hertelt <bryanhertelt@gmail.com>
2026-04-14 18:11:29 +0200

    fix: Delete toasters.

--- web-frontend/src/app/data-table-test/page.tsx
+++ web-frontend/src/app/data-table-test/page.tsx
@@ -1,16 +1,11 @@
 "use client";
 
 import { DataTable } from "@/features/data-table";
-import { ToastProvider } from "@/shared/ui/feedback/toast/ui/toast-context";
-import { ToastContainer } from "@/shared/ui/feedback/toast/ui/toast-container";
 
 export default function DataTablePage() {
   return (
-    <ToastProvider>
       <div className="flex flex-row w-screen h-screen items-center justify-center bg-black">
         <DataTable />
       </div>
-      <ToastContainer />
-    </ToastProvider>
   );
 }
--- web-frontend/src/shared/ui/feedback/toast/index.ts
+++
@@ -1,3 +0,0 @@
-export { ToastContainer } from "../toast/ui/toast-container";
-export { useToast } from "../toast/ui/toast-context";
-export { ToastProvider } from "../toast/ui/toast-context";
